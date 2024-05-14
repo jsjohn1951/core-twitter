f0a253da2f2e658b64cff6f893f3b5360967be0e
jsjohn1951 <jsjohn1951@gmail.com>
2024-05-14 15:28:18 +0400

    second commit, add csv creation

--- script.py
+++ script.py
@@ -1,54 +1,84 @@
 from tweety import Twitter
 import os
 from dotenv import load_dotenv
+import csv
 
 load_dotenv();
 USERNAME = os.getenv('USERNAME');
 PASSWORD = os.getenv('PASSWORD');
+TREND_STR = os.getenv('TRENDS');
   
-app = Twitter("session")
-app.sign_in(username=USERNAME, password=PASSWORD)
+app = Twitter("session");
+app.sign_in(username=USERNAME, password=PASSWORD);
+i = 0;
+data = []
+trends = [];
 
-trends = app.get_trends()
+print('\n\x1B[34mLoading Trends\x1B[0m:\n');
 
-i = 0
+trends_list = TREND_STR.split(',');
+for trend in trends_list:
+	trends.append(trend.strip());
+	print(i, ') \x1B[31m', trend.strip(), '\x1B[0m');
+	i = i + 1;
 
-print('Starting\n')
+print('\n\x1B[35mStarting Lookup\x1B[0m:\n');
+
+i = 0;
 
 try :
 	for trend in trends :
-		tweets = app.search(trend.name)
-		print(tweets)
+		tweets = app.search(trend);
+		for tweet in tweets :
+			username = '';
+			username = tweet.author.username;
+			userinfo = app.get_user_info(username=username);
+			data.append({
+				'Found on Trend' : trend,
+				'id' : userinfo.id,
+				'name' : userinfo.name,
+				'username' : userinfo.username,
+				'profile_img' : userinfo.profile_image_url_https,
+				'created_at' : userinfo.created_at,
+				'verified' : userinfo.verified,
+				'description' : userinfo.description,
+				'location' : userinfo.location,
+				'entities' : userinfo.entities,
+				'public_metrics' : 
+				{
+					'tweet_count' : userinfo.media_count,
+					'listed_count' : userinfo.listed_count,
+					'followers_count' : userinfo.followers_count
+				},
+				'protected' : userinfo.protected,
+				});
+
 		if ( i > 5 ):
 			break ;
 		i = i + 1;
 except Exception as error:
-	print('Exception occured: ', error)
-# for tweet in tweets:
-#     username = ''
-#     username = tweet.author.username
-#     userInfo = app.get_user_info(username=username)
-    # app.get_user
-    # userInfo.followers_count
-    # userInfo.description
-    # userInfo.location
-    # print(username, ' ---- ')
-    # print( userInfo.profile_image_url_https)
-    # userInfo._raw
-    # print(userInfo._raw)
-    # userInfo.profile_url
-    # userInfo.profile_image_url_https
-    # userInfo.fast_followers_count
-    # print('checking: ', username, '\n\n')
-    # followers = app.get_user_followers(username=username)
-    # print('followers: ', followers);
-    # time.sleep(3)
-    # scraper = Nitter(log_level=1, skip_instance_check=False)
-    # profile = scraper.get_profile_info(username)
-    # print(profile)
-    # print('type: ', type(username))
-    
-# scraper = Nitter(log_level=1, skip_instance_check=False)
-# profile = scraper.get_profile_info('WildernessWypt')
-# print(profile)
+	print('Exception occured: ', error);
 
+print('Writing csvfile');
+
+fields = [
+			'id',
+			'name',
+			'username',
+			'profile_img',
+			'created_at',
+			'verified',
+			'description',
+			'location',
+			'entities'
+			'public_metrics',
+			'protected',
+		];
+filename = os.getenv('CSV_FILENAME');
+
+with open(filename, 'w') as csvfile:
+	writer = csv.DictWriter(csvfile, fieldnames=fields);
+	writer.writeheader();
+	writer.writerows(data);
+
+print('Ended\n');
