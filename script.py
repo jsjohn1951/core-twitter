338ce150245a1cf1d7c817501922265ad3908964
jsjohn1951 <jsjohn1951@gmail.com>
2024-05-14 16:28:19 +0400

    modified the workflow to write the information as it goes

--- script.py
+++ script.py
@@ -3,16 +3,35 @@ import os
 from dotenv import load_dotenv
 import csv
 
+# load the env file
 load_dotenv();
+
+# Retrive variables
 USERNAME = os.getenv('USERNAME');
 PASSWORD = os.getenv('PASSWORD');
 TREND_STR = os.getenv('TRENDS');
-  
+FILENAME = os.getenv('CSV_FILENAME');
+
+# Global Variables
 app = Twitter("session");
 app.sign_in(username=USERNAME, password=PASSWORD);
 i = 0;
 data = []
 trends = [];
+fields = [
+			'Found on Trend',
+			'id',
+			'name',
+			'username',
+			'profile_img',
+			'created_at',
+			'verified',
+			'description',
+			'location',
+			'entities',
+			'public_metrics',
+			'protected',
+		];
 
 print('\n\x1B[34mLoading Trends\x1B[0m:\n');
 
@@ -27,58 +46,40 @@ print('\n\x1B[35mStarting Lookup\x1B[0m:\n');
 i = 0;
 
 try :
-	for trend in trends :
-		tweets = app.search(trend);
-		for tweet in tweets :
-			username = '';
-			username = tweet.author.username;
-			userinfo = app.get_user_info(username=username);
-			data.append({
-				'Found on Trend' : trend,
-				'id' : userinfo.id,
-				'name' : userinfo.name,
-				'username' : userinfo.username,
-				'profile_img' : userinfo.profile_image_url_https,
-				'created_at' : userinfo.created_at,
-				'verified' : userinfo.verified,
-				'description' : userinfo.description,
-				'location' : userinfo.location,
-				'entities' : userinfo.entities,
-				'public_metrics' : 
-				{
-					'tweet_count' : userinfo.media_count,
-					'listed_count' : userinfo.listed_count,
-					'followers_count' : userinfo.followers_count
-				},
-				'protected' : userinfo.protected,
-				});
-
-		if ( i > 5 ):
-			break ;
-		i = i + 1;
+	with open(FILENAME, 'w') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=fields);
+		writer.writeheader();
+		for trend in trends :
+			tweets = app.search(trend);
+			for tweet in tweets :
+				username = '';
+				username = tweet.author.username;
+				userinfo = app.get_user_info(username=username);
+				if userinfo.verified and userinfo.followers_count >= 5000:
+					print('Trend: ', trend, '\nUser: ', userinfo.username);
+					writer.writerow({
+						'Found on Trend' : trend,
+						'id' : userinfo.id,
+						'name' : userinfo.name,
+						'username' : userinfo.username,
+						'profile_img' : userinfo.profile_image_url_https,
+						'created_at' : userinfo.created_at,
+						'verified' : userinfo.verified,
+						'description' : userinfo.description,
+						'location' : userinfo.location,
+						'entities' : userinfo.entities,
+						'public_metrics' : 
+						{
+							'tweet_count' : userinfo.media_count,
+							'listed_count' : userinfo.listed_count,
+							'followers_count' : userinfo.followers_count
+						},
+						'protected' : userinfo.protected,
+						});
+			if ( i > 5 ):
+				break ;
+			i = i + 1;
 except Exception as error:
 	print('Exception occured: ', error);
-
-print('Writing csvfile');
-
-fields = [
-			'id',
-			'name',
-			'username',
-			'profile_img',
-			'created_at',
-			'verified',
-			'description',
-			'location',
-			'entities'
-			'public_metrics',
-			'protected',
-		];
-filename = os.getenv('CSV_FILENAME');
-
-with open(filename, 'w') as csvfile:
-	writer = csv.DictWriter(csvfile, fieldnames=fields);
-	writer.writeheader();
-	writer.writerows(data);
 
 print('Ended\n');
